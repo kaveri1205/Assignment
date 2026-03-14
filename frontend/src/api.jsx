b53c05b0b7a9262d78573b92a312d24f5e03008d
Kaver <23eg105v09@anurag.edu.in>
2026-03-14 14:03:54 +0530

    fixx1

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:7070";
+const API_BASE_URL = "https://assignment-oiak.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
