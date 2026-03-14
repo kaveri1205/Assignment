8fa8e12d79eacffd40de0e3dd323679aef866a55
Kaver <23eg105v09@anurag.edu.in>
2026-03-14 15:00:40 +0530

    fixxx

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://assignment-gv11.onrender.com",
+  baseURL: "https://assignment-gv11.onrender.com/api",
   headers: {
     "Content-Type": "application/json"
   }
